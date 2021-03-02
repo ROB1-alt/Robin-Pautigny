be7c98239557c88cb7eb6541509ead5edd4e1926
Rob1 <robin.pautigny@hetic.net>
2021-03-02 10:18:04 +0100

    the download normally works properly ↓

--- src/main.js
+++ src/main.js
@@ -1,41 +1,6 @@
-const openModalButton = document.querySelectorAll('[data-modal-target]')
-const closeModalButton = document.querySelectorAll('[data-close-btn]')
-const overlay = document.getElementById('overlay')
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
-
-
-openModalButton.forEach(button =>{
-    button.addEventListener('click', () =>{
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
-
-overlay.addEventListener('click', ()=>{
-    const modals = document.querySelectorAll('.modal.active')
-    modals.forEach(modal => {
-        closeModal(modal)
-    })
-})
-
-closeModalButton.forEach(button =>{
-    button.addEventListener('click', () =>{
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
-function openModal(modal){
-    if (modal == null) return
-    modal.classList.add('active')
-    overlay.classList.add('active')
-}
-function closeModal(modal){
-    if (modal == null) return
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
-}
 
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
